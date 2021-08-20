0a862f1c9dc7214213ca04747d6a56d6ab9227f3
KarakchievPetr <85747887+KarakchievPetr@users.noreply.github.com>
2021-08-21 00:06:43 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_butt_add_to_cart(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     assert len(browser.find_elements_by_id("add_to_basket_form")) >0, \
         'Кнопка добавления товара в корзину отсутсвует'
